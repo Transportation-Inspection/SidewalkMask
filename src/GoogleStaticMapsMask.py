f094b39f85b83f61b2ec6b5c93da665e21ca5366
Kotaro Hara <koe.bluebear@gmail.com>
2016-11-08 12:21:32 -0500

    Added road mask

--- src/GoogleStaticMapsMask.py
+++ src/GoogleStaticMapsMask.py
@@ -1,3 +1,5 @@
+import numpy as np
+import os
 import rasterio
 from geoalchemy2.shape import to_shape
 from rasterio import features, Affine
@@ -23,8 +25,16 @@ class GoogleStaticMapsMask(object):
         affine_transform = affine_scale * affine_translate
         return affine_transform
 
-    def save_google_static_maps_mask_image(self):
+    def save_google_static_maps_mask_image(self, output_dir="../data/output/google_static_maps/masks/"):
+        """Save the mask image"""
+        if not os.path.isdir(output_dir):
+            os.makedirs(output_dir)
+
         bounding_box = self.google_static_maps.get_image_bounding_box()
+
+        # Get road polygons
+        query = RoadPolygon.fetch_polygons_intersecting(bounding_box)
+        road_polygons = [to_shape(poly.geom) for poly in query]
 
         # Get sidewalk polygons
         query = SidewalkPolygon.fetch_polygons_intersecting(bounding_box)
@@ -42,36 +52,18 @@ class GoogleStaticMapsMask(object):
 
         sidewalk_polygons = [transform(sidewalk_projection, polygon) for polygon in sidewalk_polygons]
 
+        # Rasterize polygons
         affine_transform = self._get_affine_transform(bounding_box=bounding_box)
+        inverse_transform = affine_transform.__invert__()
 
+        road_raster = self._rasterize_polygons(road_polygons, inverse_transform)
+        road_raster *= 1
+        sidewalk_raster = self._rasterize_polygons(sidewalk_polygons, inverse_transform)
+        sidewalk_raster *= 2
 
-        # lng1, lng2 = bounding_box.west, bounding_box.east
-        # lat1, lat2 = bounding_box.south, bounding_box.north
-        # x_scale = self.google_static_maps.image_w / (lng2 - lng1)
-        # y_scale = - self.google_static_maps.image_h / (lat2 - lat1)
-        #
-        # affine_translate = Affine.translation(-lng1, -lat2)
-        # affine_scale = Affine.scale(x_scale, y_scale)
-        # # affine_mirror = Affine(1, 0, 0, 0, -1, image_h)
-        # affine_transform = affine_scale * affine_translate
+        road_raster[np.where(sidewalk_raster > 0)] = sidewalk_raster[np.where(sidewalk_raster > 0)]
 
-        self._rasterize_sidewalk_polygons(
-            sidewalk_polygons,
-            affine_transform.__invert__()
-        )
-
-    def _rasterize_sidewalk_polygons(self, sidewalk_polygons, affine,
-                                     output_dir="../data/output/google_static_maps/masks/"):
-        out = features.rasterize(
-            sidewalk_polygons,
-            transform=affine,
-            out_shape=(
-                self.google_static_maps.image_w,
-                self.google_static_maps.image_h
-            )
-        )
-        out *= 255
-
+        # Save file
         identifier = self.google_static_maps.get_identifier()
         filename = output_dir + identifier + '.png'
         with rasterio.open(
@@ -82,9 +74,22 @@ class GoogleStaticMapsMask(object):
                 count=1,
                 width=self.google_static_maps.image_w,
                 height=self.google_static_maps.image_h,
-                transform=affine) as dst:
-            dst.write(out, indexes=1)
+                transform=inverse_transform) as dst:
+            dst.write(road_raster, indexes=1)
         return
+
+    def _rasterize_polygons(self, polygons, affine):
+        out = features.rasterize(
+            polygons,
+            transform=affine,
+            out_shape=(
+                self.google_static_maps.image_w,
+                self.google_static_maps.image_h
+            )
+        )
+        return out
+
+
 
 if __name__ == '__main__':
     gsm = GoogleStaticMaps(38.910779, -77.046662)
