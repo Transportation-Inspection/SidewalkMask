d2cfcb3223da98d83d910600748e989b01e62290
Kotaro Hara <koe.bluebear@gmail.com>
2016-11-10 16:17:10 -0500

    update

--- src/download_gmaps_data.py
+++ src/download_gmaps_data.py
@@ -1,6 +1,7 @@
 from GoogleStaticMaps import GoogleStaticMaps
 from GoogleStaticMapsMask import GoogleStaticMapsMask
 
+
 def test():
     gsm = GoogleStaticMaps(38.910779, -77.046662)
     gsmm = GoogleStaticMapsMask(gsm)
@@ -39,9 +40,9 @@ def main():
         gsm = GoogleStaticMaps(latlng[0], latlng[1])
         gsmm = GoogleStaticMapsMask(gsm)
 
-        # gsm.fetch_google_static_maps_image()
-        # gsm.save_meta_data()
-        gsmm.save_google_static_maps_mask_image()
+        gsm.fetch_google_static_maps_image()
+        gsm.save_meta_data()
+        # gsmm.save_google_static_maps_mask_image()
 
 if __name__ == '__main__':
     main()
