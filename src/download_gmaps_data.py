3c102ebe6fc9c2d64de27cc6c9bce00982a02c0e
Kotaro Hara <koe.bluebear@gmail.com>
2016-11-18 10:22:22 -0500

    Data processing script

--- src/download_gmaps_data.py
+++ src/download_gmaps_data.py
@@ -11,7 +11,8 @@ from shapely.ops import transform
 from functools import partial
 import pyproj
 
-from scipy.spatial import KDTree
+from rtree import index
+import math
 
 def test():
     gsm = GoogleStaticMaps(38.910779, -77.046662)
@@ -57,20 +58,66 @@ def test():
 
 def data_1000():
     # Latlngs from ST_MakeEnvelope(-77.040, 38.890, -77.005, 38.911, 4326)
-    project = partial(
+    distance_threshold_m = 80  # meters
+    project_4326_to_26917 = partial(
         pyproj.transform,
         pyproj.Proj(init='EPSG:4326'),
         pyproj.Proj(init='EPSG:26917')
     )
 
+    project_26917_to_4326 = partial(
+        pyproj.transform,
+        pyproj.Proj(init='EPSG:26917'),
+        pyproj.Proj(init='EPSG:4326')
+    )
+
     bounding_box = BoundingBox(38.890, -77.040, 38.911, -77.005)
     query = StreetEdge.fetch_street_edges_intersecting(bounding_box)
 
+    # rtree index
+    # rt_idx = RTreeIndex()
+
     segments = [to_shape(segment.geom) for segment in query]
-    # to_shape(poly.geom)
     for segment in segments:
-        transformed_segment = transform(project, segment)
-        print(transformed_segment.length)
+        projected_segment = transform(project_4326_to_26917, segment)
+        length_m = projected_segment.length
+        num_split = math.ceil(length_m / distance_threshold_m)
+
+
+        piece_length_m = length_m / num_split
+        print(length_m, piece_length_m)
+
+        for idx in range(num_split + 1):
+            p = projected_segment.interpolate(idx * piece_length_m)
+
+            print(p)
+
+        print()
+
+        # num_split = math.ceil(transformed_segment.length / distance_threshold_m) + 1
+
+class RTreeIndex(object):
+    def __init__(self):
+        self.idx = index.Index()
+        self.counter = 1
+
+    def insert(self, point):
+        bounds = (point.x, point.y, point.x, point.y)
+        self.idx.insert(self.counter, bounds=bounds, obj=point)
+        self.counter += 1
+
+    def query_points_within(self, point, distance_m=40):
+        x_min = point.x - distance_m / 2
+        x_max = point.x + distance_m / 2
+        y_min = point.y - distance_m / 2
+        y_max = point.y + distance_m / 2
+        bounds = (x_min, y_min, x_max, y_max)
+        result = self.idx.intersection(bounds, object=True)
+
+        def dist(x, y):
+            return math.sqrt((point.x - x) ** 2 + (point.y - y) ** 2)
+
+        []
 
 
 if __name__ == '__main__':
